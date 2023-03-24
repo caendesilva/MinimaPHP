4a25e7bae88e0e7f8a0c1823e8a423c8365d23c6
Caen De Silva <caen@desilva.se>
2023-03-24 21:02:41 +0100

    Fix wrong case

--- examples/arguments.php
+++ examples/arguments.php
@@ -5,5 +5,5 @@ require_once 'minima.php';
 Command::main(function () {
     $this->info('Here is the data you passed to the command:');
     $this->line('Options:' . implode(', ', $this->options));
-    $this->line('Arguments:' . implode(', ', $this->Arguments));
+    $this->line('Arguments:' . implode(', ', $this->arguments));
 });
