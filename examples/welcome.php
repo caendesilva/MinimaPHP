6d246d7659a01dc222fb827449bcd025260dd7ee
Caen De Silva <caen@desilva.se>
2023-03-24 20:42:04 +0100

    Add void as the function return type

--- examples/welcome.php
+++ examples/welcome.php
@@ -2,7 +2,7 @@
 
 require_once 'minima.php';
 
-$exitCode = Command::main(function () {
+$exitCode = Command::main(function (): void {
     $this->formatted('<info>Welcome to MinimaPHP!</info> <comment>You are running version '.Minima::VERSION.'.</comment>');
 });
 
