90b80f8672cc0531eb3be5903afebccf5f511b20
Caen De Silva <caen@desilva.se>
2023-03-24 21:08:22 +0100

    Document examples

--- examples/arguments.php
+++ examples/arguments.php
@@ -7,3 +7,9 @@ Command::main(function () {
     $this->line('Options: ' . implode(', ', $this->options));
     $this->line('Arguments: ' . implode(', ', $this->arguments));
 });
+
+// For example: `php examples/arguments.php example --help -v --foo=bar bar=baz`
+// Will return:
+//   Here is the data you passed to the command:
+//   Options: --help, -v, --foo=bar
+//   Arguments: example, bar=baz
