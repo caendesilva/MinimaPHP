74b884d4ddafdf80f4f8338c07e0255613c5cfad
Caen De Silva <caen@desilva.se>
2023-03-24 19:55:32 +0100

    Create FileFormatter.php

---
+++ tests/FileFormatter.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once __DIR__ . '/../src/minima.php';
+
+Command::main(function () {
+    $this->info('Formatting files!');
+
+    $directories = [
+        __DIR__ . '/../src',
+        __DIR__ . '/../tests',
+        __DIR__ . '/../examples'
+    ];
+
+    $allFiles = [];
+    $files = glob(__DIR__ . '/../*.php', GLOB_BRACE);
+
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            $allFiles[] = $file;
+        }
+    }
+
+    foreach ($directories as $dir) {
+        $files = glob($dir . '/**/*', GLOB_BRACE);
+
+        foreach ($files as $file) {
+            if (is_file($file)) {
+                $allFiles[] = $file;
+            }
+        }
+
+        $files = glob($dir . '/*', GLOB_BRACE);
+
+        foreach ($files as $file) {
+            if (is_file($file)) {
+                $allFiles[] = $file;
+            }
+        }
+    }
+
+    $files = $allFiles;
+
+    foreach ($files as $file) {
+        $this->line("Formatting ".basename($file));
+        formatFile($file);
+    }
+});
+
+function formatFile(string $file): void {
+    $contents = str_replace("\r\n", "\n", file_get_contents($file));
+    $contents = str_replace("\t", "    ", $contents);
+
+    $lines = explode("\n", $contents);
+
+    foreach ($lines as $index => $line) {
+        $lines[$index] = rtrim($line);
+    }
+    file_put_contents($file, implode("\n", $lines));
+}
