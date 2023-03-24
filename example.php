4f2347d7dd93c440cc8f8b6562a318167c7b4fe1
Caen De Silva <caen@desilva.se>
2023-03-24 17:45:12 +0100

    Use class data instead of dependency injection

--- example.php
+++ example.php
@@ -2,8 +2,8 @@
 
 require_once 'src/minima.php';
 
-$exitCode = Command::main(function (Command $command) {
-	Console::line('Hello World!');
+$exitCode = Command::main(function () {
+	$this->console->line('Hello World!');
 });
 
 exit($exitCode);
