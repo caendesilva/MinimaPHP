a537ca7f3c3b9eab7518236bc9f16ce50f609e14
Caen De Silva <caen@desilva.se>
2023-03-24 19:28:34 +0100

    Test WritesToConsole

--- tests/Unit.php
+++ tests/Unit.php
@@ -8,6 +8,19 @@ $exitCodes = [
 	Command::main(function () {
 		$this->formatted('<info>Running unit tests!</info>');
 	}),
+
+	Test::main(function () {
+		$this->info('Testing: WritesToConsole');
+
+		$this->line('---------');
+		$this->write('write');
+		$this->line('line');
+		$this->info('info');
+		$this->warning('warning');
+		$this->error('error');
+		$this->formatted('formatted');
+		$this->line('---------');
+	}),
 ];
 
 exit(max($exitCodes));
