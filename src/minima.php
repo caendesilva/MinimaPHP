3ecfe3cc97bb2604bd18874bbe513b8078e10b19
Caen De Silva <caen@desilva.se>
2023-03-24 17:11:48 +0100

    Add ANSI interface

--- src/minima.php
+++ src/minima.php
@@ -1,1 +1,13 @@
 <?php
+
+interface ANSI {
+    const BLACK = "\033[30m";
+    const RED = "\033[31m";
+    const GREEN = "\033[32m";
+    const YELLOW = "\033[33m";
+    const BLUE = "\033[34m";
+    const MAGENTA = "\033[35m";
+    const CYAN = "\033[36m";
+    const WHITE = "\033[37m";
+    const RESET = "\033[0m";
+}
