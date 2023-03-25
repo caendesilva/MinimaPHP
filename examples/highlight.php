8ff86af5b70e8a256d5e5585a13491dc03e6a05a
Caen De Silva <caen@desilva.se>
2023-03-25 19:37:01 +0100

    Make static helpers instance

--- examples/highlight.php
+++ examples/highlight.php
@@ -25,19 +25,19 @@ class Dumper {
 
     protected function runHighlighter(mixed $data): string {
         if (is_null($data)) {
-            return static::null($data);
+            return $this->null($data);
         }
         if (is_string($data)) {
-            return static::string($data);
+            return $this->string($data);
         }
         if (is_int($data)) {
-            return static::int($data);
+            return $this->int($data);
         }
         if (is_bool($data)) {
-            return static::bool($data);
+            return $this->bool($data);
         }
         if (is_array($data)) {
-            return static::array($data);
+            return $this->array($data);
         }
         if (is_object($data)) {
             return $data::class;
@@ -46,23 +46,23 @@ class Dumper {
         return (string) $data;
     }
 
-    protected static function null(null|string $value): string {
+    protected function null(null|string $value): string {
         return 'null';
     }
 
-    protected static function string(string $value): string {
+    protected function string(string $value): string {
         return "'$value'";
     }
 
-    protected static function int(int $value): string {
+    protected function int(int $value): string {
         return $value;
     }
 
-    protected static function bool(bool $value): string {
+    protected function bool(bool $value): string {
         return $value ? 'true' : 'false';
     }
 
-    protected static function array(array $array): string {
+    protected function array(array $array): string {
         $parts = [];
         foreach ($array as $key => $value) {
             if (is_int($key)) {
