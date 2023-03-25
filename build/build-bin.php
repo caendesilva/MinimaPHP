6ffbf2ed62817120c67b4b0868eb604b290f6343
Caen De Silva <caen@desilva.se>
2023-03-25 21:35:18 +0100

    Exit gracefully if no code lines changed since last build

--- build/build-bin.php
+++ build/build-bin.php
@@ -11,6 +11,11 @@ $exitCode = Command::main(function (): void {
     $src = str_replace("<?php", "// start minima.php", $src);
     $bin = str_replace("require_once __DIR__.'/../minima.php';", $src . "\n// end minima.php", $base);
 
+    if (isFileChanged($bin, file_get_contents(__DIR__.'/../bin/minima'))) {
+        $this->info('No changes made since last build. Exiting gracefully.');
+        return;
+    }
+
     $shortSha = trim(shell_exec('git rev-parse --short HEAD'));
     $bin = str_replace("const REVISION = 'dev'", "const REVISION = '$shortSha'", $bin);
 
@@ -18,5 +23,20 @@ $exitCode = Command::main(function (): void {
 
     $this->info('All done!');
 });
+
+function isFileChanged(string $new, string $old): bool {
+    $unsetRevisionLines = function(string $contents): string {
+        $lines = explode("\n", $contents);
+        foreach ($lines as $index => $line) {
+            if (str_contains($line, 'const REVISION = ')) {
+                unset($lines[$index]);
+                break;
+            }
+        }
+        return implode("\n", $lines);
+    };
+
+    return $unsetRevisionLines($new) === $unsetRevisionLines($new);
+}
 
 exit($exitCode);
