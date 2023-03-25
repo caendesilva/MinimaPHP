b25bb82c5131345bcbe3e1339abd71d43f6a2070
Caen De Silva <caen@desilva.se>
2023-03-25 18:57:27 +0100

    Revert "Add highlight function"
    This reverts commit 7457eac173804646e54a82d628fd79006d64a25f.

--- minima.php
+++ minima.php
@@ -207,12 +207,6 @@ if (! function_exists('main')) {
     }
 }
 
-if (! function_exists('highlight')) {
-    function highlight(mixed $value): void {
-        return Highlighter::highlight($value);
-    }
-}
-
 if (! function_exists('dump')) {
     function dump(mixed $value): void {
         var_dump($value);
