f7a64c252594c87c1e1dc4b468f07459a68695b5
Caen De Silva <caen@desilva.se>
2023-03-25 21:25:50 +0100

    Replace the revision with the Git short SHA

--- build/build-bin.php
+++ build/build-bin.php
@@ -11,6 +11,9 @@ $exitCode = Command::main(function (): void {
     $src = str_replace("<?php", "// start minima.php", $src);
     $bin = str_replace("require_once __DIR__.'/../minima.php';", $src . "\n// end minima.php", $base);
 
+    $shortSha = trim(shell_exec('git rev-parse --short HEAD'));
+    $bin = str_replace("const REVISION = 'dev'", "const REVISION = '$shortSha'", $bin);
+
     file_put_contents(__DIR__.'/../bin/minima', $bin);
 
     $this->info('All done!');
