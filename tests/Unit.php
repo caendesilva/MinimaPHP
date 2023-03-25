741fe7cd7f15a511d5a5799c4fe79c9bc513c405
Caen De Silva <caen@desilva.se>
2023-03-25 13:34:29 +0100

    Refactor to use PicoUnit test case

--- tests/Unit.php
+++ tests/Unit.php
@@ -1,51 +1,52 @@
 <?php
 
 require_once __DIR__ . '/../minima.php';
+require_once __DIR__ . '/PicoUnit.php';
 
-use Command as Test;
+use PicoUnit as TestCase;
 
-$exitCodes = [
-    Command::main(function () {
-        $this->formatted('<info>Running unit tests!</info>');
-    }),
+TestCase::boot(__FILE__);
 
-    Test::main(function () {
-        $this->info('Testing: ANSI Interface');
+Command::main(function () {
+    $this->formatted('<info>Running unit tests!</info>');
+});
 
-        echo (assert(ANSI::BLACK === "\033[30m") ? 'Assertion passed' : 'Assertion failed') . PHP_EOL;
-        echo (assert(ANSI::RED === "\033[31m") ? 'Assertion passed' : 'Assertion failed') . PHP_EOL;
-        echo (assert(ANSI::GREEN === "\033[32m") ? 'Assertion passed' : 'Assertion failed') . PHP_EOL;
-        echo (assert(ANSI::YELLOW === "\033[33m") ? 'Assertion passed' : 'Assertion failed') . PHP_EOL;
-        echo (assert(ANSI::BLUE === "\033[34m") ? 'Assertion passed' : 'Assertion failed') . PHP_EOL;
-        echo (assert(ANSI::MAGENTA === "\033[35m") ? 'Assertion passed' : 'Assertion failed') . PHP_EOL;
-        echo (assert(ANSI::CYAN === "\033[36m") ? 'Assertion passed' : 'Assertion failed') . PHP_EOL;
-        echo (assert(ANSI::WHITE === "\033[37m") ? 'Assertion passed' : 'Assertion failed') . PHP_EOL;
-        echo (assert(ANSI::GRAY === "\033[90m") ? 'Assertion passed' : 'Assertion failed') . PHP_EOL;
-        echo (assert(ANSI::RESET === "\033[0m") ? 'Assertion passed' : 'Assertion failed') . PHP_EOL;
-    }),
+Command::main(function () {
+    $this->info('Testing: ANSI Interface');
 
-    Test::main(function () {
-        $this->info('Testing: XML_ANSI Interface');
+    test(ANSI::BLACK === "\033[30m");
+    test(ANSI::RED === "\033[31m");
+    test(ANSI::GREEN === "\033[32m");
+    test(ANSI::YELLOW === "\033[33m");
+    test(ANSI::BLUE === "\033[34m");
+    test(ANSI::MAGENTA === "\033[35m");
+    test(ANSI::CYAN === "\033[36m");
+    test(ANSI::WHITE === "\033[37m");
+    test(ANSI::GRAY === "\033[90m");
+    test(ANSI::RESET === "\033[0m");
+});
 
-        echo (assert(XML_ANSI::INFO === ANSI::GREEN) ? 'Assertion passed' : 'Assertion failed') . PHP_EOL;
-        echo (assert(XML_ANSI::WARNING === ANSI::YELLOW) ? 'Assertion passed' : 'Assertion failed') . PHP_EOL;
-        echo (assert(XML_ANSI::ERROR === ANSI::RED) ? 'Assertion passed' : 'Assertion failed') . PHP_EOL;
-        echo (assert(XML_ANSI::COMMENT === ANSI::GRAY) ? 'Assertion passed' : 'Assertion failed') . PHP_EOL;
-        echo (assert(XML_ANSI::RESET === ANSI::RESET) ? 'Assertion passed' : 'Assertion failed') . PHP_EOL;
-   }),
+Command::main(function () {
+    $this->info('Testing: XML_ANSI Interface');
 
-    Test::main(function () {
-        $this->info('Testing: WritesToConsole');
+    test(XML_ANSI::INFO === ANSI::GREEN);
+    test(XML_ANSI::WARNING === ANSI::YELLOW);
+    test(XML_ANSI::ERROR === ANSI::RED);
+    test(XML_ANSI::COMMENT === ANSI::GRAY);
+    test(XML_ANSI::RESET === ANSI::RESET);
+});
 
-        $this->line('---------');
-        $this->write('write');
-        $this->line('line');
-        $this->info('info');
-        $this->warning('warning');
-        $this->error('error');
-        $this->formatted('formatted');
-        $this->line('---------');
-    }),
-];
+Command::main(function () {
+    $this->info('Testing: WritesToConsole');
 
-exit(max($exitCodes));
+    $this->line('---------');
+    $this->write('write');
+    $this->line('line');
+    $this->info('info');
+    $this->warning('warning');
+    $this->error('error');
+    $this->formatted('formatted');
+    $this->line('---------');
+});
+
+exit(TestCase::stop());
