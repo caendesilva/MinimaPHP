9d8c131de4bb77993792a899b1302aaff76e88a3
Caen De Silva <caen@desilva.se>
2023-03-25 18:58:57 +0100

    Add base class scaffold

--- examples/highlight.php
+++ examples/highlight.php
@@ -5,3 +5,9 @@ require_once 'minima.php';
 Command::main(function (): void {
     $this->formatted('<info>Simple console highlighting data dumper</info>');
 });
+
+class Dumper {
+    public static function Highlight(mixed $data): string {
+        return //
+    }
+}
