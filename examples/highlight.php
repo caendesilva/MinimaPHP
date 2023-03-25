1c41082205cf3249d1756d6f6385c87171440f41
Caen De Silva <caen@desilva.se>
2023-03-25 19:02:10 +0100

    Add sample output

--- examples/highlight.php
+++ examples/highlight.php
@@ -4,6 +4,16 @@ require_once 'minima.php';
 
 Command::main(function (): void {
     $this->formatted('<info>Simple console highlighting data dumper</info>');
+
+    $this->line(Dumper::highlight([
+        'foo' => 'bar',
+        'array' => [
+            'some',
+            'contents',
+            'bool' => true,
+            'integer' => 10,
+        ],
+    ]));
 });
 
 class Dumper {
