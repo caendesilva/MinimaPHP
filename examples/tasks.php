5cc68cc24f59bdc40542812e8e6cb7f60ad7629d
Caen De Silva <caen@desilva.se>
2023-03-26 14:22:38 +0200

    Invert wrongly named variable

--- examples/tasks.php
+++ examples/tasks.php
@@ -3,13 +3,13 @@
 require_once 'minima.php';
 
 Command::main(function (): void {
-    $runTasks = $this->hasOption('skip-tasks') || $this->hasOption('s');
+    $skipTasks = $this->hasOption('skip-tasks') || $this->hasOption('s');
 
-    if (! $runTasks) {
+    if ($skipTasks) {
         putenv('SKIP_TASKS=true');
     }
 
-    $taskMessage = $runTasks ? "remove `--skip-tasks` to run the tasks" : "pass `--skip-tasks` to skip the tasks";
+    $taskMessage = $skipTasks ? "remove `--skip-tasks` to run the tasks" : "pass `--skip-tasks` to skip the tasks";
     $this->formatted("<info>Tasks example</info> - <yellow>$taskMessage</yellow>");
 
     task('myTask', function () {
