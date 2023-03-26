c794f6db9303982089b4b3d6fdfed41a00a16637
Caen De Silva <caen@desilva.se>
2023-03-26 21:50:19 +0200

    Test the XML formatting method

--- tests/Unit/InputOutputHelpersTest.php
+++ tests/Unit/InputOutputHelpersTest.php
@@ -25,6 +25,17 @@ test('WritesToOutput helper methods', function () {
 ');
 });
 
+test('WritesToOutput XML formatting method', function () {
+    ob_start();
+    $output = new InteractsWithIOClass;
+    $output->formatted('<info>info</info>, <warning>warning</warning>, <error>error</error>, <comment>comment</comment>, <reset>reset</reset>');
+    $output->formatted('<red>red</red>, <green>green</green>, <blue>blue</blue>, <yellow>yellow</yellow>, <magenta>magenta</magenta>, <cyan>cyan</cyan>');
+
+    expect(ob_get_clean())->toBe('[32minfo[0m, [33mwarning[0m, [31merror[0m, [90mcomment[0m, [0mreset[0m
+[31mred[0m, [32mgreen[0m, [34mblue[0m, [33myellow[0m, [35mmagenta[0m, [36mcyan[0m
+');
+});
+
 class InteractsWithIOClass {
     use InteractsWithIO;
 }
