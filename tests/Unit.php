a8199e840ed495fa511bef70535c71c2d70b0094
Caen De Silva <caen@desilva.se>
2023-03-26 20:31:10 +0200

    Inline PicoUnit class

--- tests/Unit.php
+++ tests/Unit.php
@@ -1,7 +1,67 @@
 <?php
 
 require_once __DIR__.'/../minima.php';
-require_once __DIR__.'/PicoUnit.php';
+
+final class PicoUnit
+{
+    private const PASSED = "\033[32m".'passed'."\033[0m";
+    private const FAILED = "\033[31m".'failed'."\033[0m";
+
+    public static string $startMessage = "Running tests for %s\n\n";
+
+    private static self $instance;
+    private float $startTime;
+    private array $contents;
+    private int $exitCode;
+
+    private function __construct(string $file)
+    {
+        $this->startTime = microtime(true);
+        $this->contents = file($file);
+        $this->exitCode = 0;
+    }
+
+    public static function boot(string $file): void
+    {
+        echo sprintf(ANSI::GREEN.self::$startMessage.ANSI::RESET, basename(dirname($file)));
+
+        self::$instance = new self($file);
+    }
+
+    public static function stop(): int
+    {
+        $stopTimeInMs = number_format((microtime(true) - self::getInstance()->startTime) * 1000, 2);
+        echo sprintf("\n%sTests completed in %s with exit code %d %s\n", ANSI::GREEN, $stopTimeInMs.'ms', self::getInstance()->exitCode, ANSI::RESET);
+
+        return self::getInstance()->exitCode;
+    }
+
+    private static function getInstance(): self
+    {
+        return self::$instance;
+    }
+
+    public static function test(bool|Closure $result): void
+    {
+        $line = trim(self::getInstance()->contents[debug_backtrace()[1]['line'] - 1]);
+        $testName = substr($line, 5, strpos($line, ');') - 5);
+
+        if ($result instanceof Closure) {
+            $result = $result();
+        }
+
+        if (!$result) {
+            self::getInstance()->exitCode = 1;
+        }
+
+        echo($result ? self::PASSED : self::FAILED).": $testName\n";
+    }
+}
+
+function test(bool|Closure $result): void
+{
+    PicoUnit::test($result);
+}
 
 use PicoUnit as TestCase;
 
