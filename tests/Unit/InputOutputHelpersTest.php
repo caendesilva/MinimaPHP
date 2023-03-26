719ef9d9a4d58cd1a36c6b0e7cb90fa2f4608d8b
Caen De Silva <caen@desilva.se>
2023-03-26 20:55:04 +0200

    GPT generate Output class unit test

--- tests/Unit/InputOutputHelpersTest.php
+++ tests/Unit/InputOutputHelpersTest.php
@@ -1,1 +1,8 @@
 <?php
+
+test('Output class writes the string to the output', function () {
+    ob_start();
+    Output::write('Hello, world!');
+    $output = ob_get_clean();
+    expect($output)->toBe('Hello, world!');
+});
