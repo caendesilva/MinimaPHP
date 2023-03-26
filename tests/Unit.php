480c5646a4cfe29a7ad2d589d2c1c1a1cd49027c
Caen De Silva <caen@desilva.se>
2023-03-26 13:46:50 +0200

    Split out colour output test

--- tests/Unit.php
+++ tests/Unit.php
@@ -10,6 +10,37 @@ TestCase::$startMessage = "Running unit tests!\n";
 TestCase::boot(__FILE__);
 
 Command::main(function () {
+    $this->formatted("\n<warning>ANSI Colours:</warning>");
+
+    $this->line('---------');
+    // $this->line('Base ANSI');
+    $this->line(ANSI::BLACK.'BLACK'. ANSI::RESET);
+    $this->line(ANSI::RED.'RED'. ANSI::RESET);
+    $this->line(ANSI::GREEN.'GREEN'. ANSI::RESET);
+    $this->line(ANSI::YELLOW.'YELLOW'. ANSI::RESET);
+    $this->line(ANSI::BLUE.'BLUE'. ANSI::RESET);
+    $this->line(ANSI::MAGENTA.'MAGENTA'. ANSI::RESET);
+    $this->line(ANSI::CYAN.'CYAN'. ANSI::RESET);
+    $this->line(ANSI::WHITE.'WHITE'. ANSI::RESET);
+    $this->line(ANSI::GRAY.'GRAY'. ANSI::RESET);
+    $this->line(ANSI::RESET.'RESET'. ANSI::RESET);
+
+    // $this->line('XML ANSI');
+    $this->line(XML_ANSI::INFO .'INFO'.ANSI::RESET);
+    $this->line(XML_ANSI::WARNING .'WARNING'.ANSI::RESET);
+    $this->line(XML_ANSI::ERROR .'ERROR'.ANSI::RESET);
+    $this->line(XML_ANSI::COMMENT .'COMMENT'.ANSI::RESET);
+    $this->line(XML_ANSI::RESET .'RESET'.ANSI::RESET);
+
+    // $this->line('Fluent methods');
+    $this->line('line');
+    $this->info('info');
+    $this->warning('warning');
+    $this->error('error');
+    $this->line('---------');
+});
+
+Command::main(function () {
     $this->formatted("\n<warning>Testing: ANSI Interface</warning>");
 
     test(ANSI::BLACK === "\033[30m");
@@ -22,18 +53,6 @@ Command::main(function () {
     test(ANSI::WHITE === "\033[37m");
     test(ANSI::GRAY === "\033[90m");
     test(ANSI::RESET === "\033[0m");
-    $this->line('---------');
-    $this->line(ANSI::BLACK.'BLACK'. ANSI::RESET);
-    $this->line(ANSI::RED.'RED'. ANSI::RESET);
-    $this->line(ANSI::GREEN.'GREEN'. ANSI::RESET);
-    $this->line(ANSI::YELLOW.'YELLOW'. ANSI::RESET);
-    $this->line(ANSI::BLUE.'BLUE'. ANSI::RESET);
-    $this->line(ANSI::MAGENTA.'MAGENTA'. ANSI::RESET);
-    $this->line(ANSI::CYAN.'CYAN'. ANSI::RESET);
-    $this->line(ANSI::WHITE.'WHITE'. ANSI::RESET);
-    $this->line(ANSI::GRAY.'GRAY'. ANSI::RESET);
-    $this->line(ANSI::RESET.'RESET'. ANSI::RESET);
-    $this->line('---------');
 });
 
 Command::main(function () {
@@ -44,13 +63,6 @@ Command::main(function () {
     test(XML_ANSI::ERROR === ANSI::RED);
     test(XML_ANSI::COMMENT === ANSI::GRAY);
     test(XML_ANSI::RESET === ANSI::RESET);
-    $this->line('---------');
-    $this->line(XML_ANSI::INFO .'INFO'.ANSI::RESET);
-    $this->line(XML_ANSI::WARNING .'WARNING'.ANSI::RESET);
-    $this->line(XML_ANSI::ERROR .'ERROR'.ANSI::RESET);
-    $this->line(XML_ANSI::COMMENT .'COMMENT'.ANSI::RESET);
-    $this->line(XML_ANSI::RESET .'RESET'.ANSI::RESET);
-    $this->line('---------');
 });
 
 Command::main(function () {
