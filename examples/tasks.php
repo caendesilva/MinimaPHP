50575f2610f9cbf17508179fd495a020d841e4d6
Caen De Silva <caen@desilva.se>
2023-03-26 14:12:44 +0200

    Add flag shorthand

--- examples/tasks.php
+++ examples/tasks.php
@@ -3,7 +3,7 @@
 require_once 'minima.php';
 
 Command::main(function (): void {
-    $runTasks = $this->hasOption('skip-tasks');
+    $runTasks = $this->hasOption('skip-tasks') || $this->hasOption('s');
 
     $taskMessage = $runTasks ? "remove `--skip-tasks` to run the tasks" : "pass `--skip-tasks` to skip the tasks";
     $this->formatted("<info>Tasks example</info> - <yellow>$taskMessage</yellow>");
