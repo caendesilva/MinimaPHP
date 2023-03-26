bf6a44fb61d737b94fedf3bf4d1ecc293e7636c6
Caen De Silva <caen@desilva.se>
2023-03-26 14:10:44 +0200

    Check if has option

--- examples/tasks.php
+++ examples/tasks.php
@@ -3,6 +3,8 @@
 require_once 'minima.php';
 
 Command::main(function (): void {
+    $runTasks = $this->hasOption('skip-tasks');
+
     $this->formatted('<info>Tasks example</info> - <yellow>pass `--no-tasks` to skip the tasks</yellow>');
 
     //
