0f280b83cbd91e40599fc2c7fff8a78b8d468595
Caen De Silva <caen@desilva.se>
2023-03-26 14:11:56 +0200

    Make message dynamic

--- examples/tasks.php
+++ examples/tasks.php
@@ -5,7 +5,8 @@ require_once 'minima.php';
 Command::main(function (): void {
     $runTasks = $this->hasOption('skip-tasks');
 
-    $this->formatted('<info>Tasks example</info> - <yellow>pass `--no-tasks` to skip the tasks</yellow>');
+    $taskMessage = $runTasks ? "remove `--no-tasks` to run the tasks" : "pass `--no-tasks` to skip the tasks";
+    $this->formatted("<info>Tasks example</info> - <yellow>$taskMessage</yellow>");
 
     //
 });
