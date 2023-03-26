861121e7c60e9c51a350dae064767b19c17dc4e8
Caen De Silva <caen@desilva.se>
2023-03-26 14:09:58 +0200

    Update start message

--- examples/tasks.php
+++ examples/tasks.php
@@ -3,7 +3,7 @@
 require_once 'minima.php';
 
 Command::main(function (): void {
-    $this->formatted('<info>Tasks example</info>');
+    $this->formatted('<info>Tasks example</info> - <yellow>pass `--no-tasks` to skip the tasks</yellow>');
 
     //
 });
