8d65d71eb29c0566ea52d649e5601c8b5a08a643
Caen De Silva <caen@desilva.se>
2023-03-26 21:56:59 +0200

    Test the highlighter highlights dumped array

--- tests/Unit/HighlighterTest.php
+++ tests/Unit/HighlighterTest.php
@@ -1,2 +1,35 @@
 <?php
 
+test('The highlighter highlights dumped array', function () {
+    $array = [
+        'foo',
+        'bar'   => 'baz',
+        'array' => [
+            'some',
+            'contents',
+            'bool'    => true,
+            'integer' => 10,
+            'deep'    => ['array'],
+        ],
+        'object' => new Dumper(),
+        'null'   => null,
+    ];
+
+    $highlighted = Dumper::highlight($array);
+
+    expect($highlighted)->toBe(('[37m[[0m
+  [34m\'[32mfoo[34m\'[0m,
+  [34m\'[32mbar[34m\'[0m => [34m\'[32mbaz[34m\'[0m,
+  [34m\'[32marray[34m\'[0m => [37m[[0m
+    [34m\'[32msome[34m\'[0m,
+    [34m\'[32mcontents[34m\'[0m,
+    [34m\'[32mbool[34m\'[0m => [31mtrue[0m,
+    [34m\'[32minteger[34m\'[0m => [33m10[0m,
+    [34m\'[32mdeep[34m\'[0m => [37m[[0m
+      [34m\'[32marray[34m\'[0m
+    [37m][0m
+  [37m][0m,
+  [34m\'[32mobject[34m\'[0m => [33mDumper[0m,
+  [34m\'[32mnull[34m\'[0m => [31mnull[0m
+[37m][0m'));
+});
