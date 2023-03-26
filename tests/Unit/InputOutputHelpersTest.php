0161aff58a842cc15bd2c86a1933c5051441fc3c
Caen De Silva <caen@desilva.se>
2023-03-26 21:07:04 +0200

    Document GPT prompt

--- tests/Unit/InputOutputHelpersTest.php
+++ tests/Unit/InputOutputHelpersTest.php
@@ -1,5 +1,7 @@
 <?php
 
+// GPT Prompt: Write PestPHP unit tests for the code provided below. You should use the `test()` function and the expectation API.
+
 test('Output class writes the string to the output', function () {
     ob_start();
     Output::write('Hello, world!');
