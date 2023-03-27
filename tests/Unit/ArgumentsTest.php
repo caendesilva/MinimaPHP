6271807a2ae21c203840802dd6842648d4e15e09
Caen De Silva <caen@desilva.se>
2023-03-27 14:08:14 +0200

    Create more unit tests

--- tests/Unit/ArgumentsTest.php
+++ tests/Unit/ArgumentsTest.php
@@ -3,3 +3,31 @@
 test('options', fn() => main(function () {
     expect($this->options)->toBeArray();
 }));
+
+test('arguments', fn() => main(function () {
+    expect($this->arguments)->toBeArray();
+}));
+
+test('has option', fn() => main(function () {
+    expect($this->hasOption('foo'))->toBeFalse();
+}));
+
+test('has argument', fn() => main(function () {
+    expect($this->hasArgument('foo'))->toBeFalse();
+}));
+
+test('get option', fn() => main(function () {
+    expect($this->getOption('foo'))->toBeNull();
+}));
+
+test('get argument', fn() => main(function () {
+    expect($this->getArgument('foo'))->toBeNull();
+}));
+
+test('get option default', fn() => main(function () {
+    expect($this->getOption('foo', 'bar'))->toBe('bar');
+}));
+
+test('get argument default', fn() => main(function () {
+    expect($this->getArgument('foo', 'bar'))->toBe('bar');
+}));
